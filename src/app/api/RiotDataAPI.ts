9448610418947f2374fa7fe039f8b89de49523e9
choichangyeon <changyon99@naver.com>
2025-03-11 12:30:28 +0900

    feat:#1 API URL 설정

---
+++ src/app/api/RiotDataAPI.ts
@@ -0,0 +1,14 @@
+// 데이터 요청 URL - JSON_URL -> .json으로 요청해야 함
+
+// 로테이션 챔피언 데이터
+const LOL_ROTATION_CHAMPION_URL: string =
+  "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations";
+// 모든 챔피언 데이터
+const LOL_CHAMPIONS_JSON_URL: string =
+  "https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion.json";
+// 특정 챔피언 데이터
+const LOL_CHAMPION_DETAILS_JSON_URL: string =
+  "https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion";
+// 모든 아이템 데이터
+const LOL_ITEMS_JSON_URL: string =
+  "https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/item.json";
