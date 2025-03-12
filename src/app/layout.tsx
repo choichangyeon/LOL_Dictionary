192dc4b6d7860ad5ca9c29f2593d0845ed05e2b2
choichangyeon <changyon99@naver.com>
2025-03-12 15:58:50 +0900

    design:#5 레이아웃 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
           <Link href={"/items"}>아이템 목록</Link>
           <Link href={"/rotation"}>챔피언 로테이션</Link>
         </header>
-        {children}
+        <main className="w-full">{children}</main>
       </body>
     </html>
   );
