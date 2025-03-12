0a38508c249f4ac3f57743bf41e00ad6b75d2644
choichangyeon <changyon99@naver.com>
2025-03-12 20:44:34 +0900

    feat:#7 아이템 정보 api

--- src/utils/serverApi.ts
+++ src/utils/serverApi.ts
@@ -1,6 +1,11 @@
 import { Champion } from "@/types/champion/Champion";
 import { ChampionsData } from "@/types/ChampionsData";
-import { LOL_CHAMPIONS_JSON_URL } from "@constants/RiotDataURL";
+import { ItemDetail } from "@/types/item/ItemDetail";
+import { ItemsData } from "@/types/ItemsData";
+import {
+  LOL_CHAMPIONS_JSON_URL,
+  LOL_ITEMS_JSON_URL,
+} from "@constants/RiotDataURL";
 
 export const getChampionsData = async (): Promise<Champion[]> => {
   const res = await fetch(LOL_CHAMPIONS_JSON_URL, {
@@ -12,4 +17,12 @@ export const getChampionsData = async (): Promise<Champion[]> => {
   const champions: Champion[] = Object.values(data.data);
 
   return champions;
+};
+
+export const getItemsData = async (): Promise<ItemDetail[]> => {
+  const res = await fetch(LOL_ITEMS_JSON_URL);
+  const data: ItemsData = await res.json();
+  const items: ItemDetail[] = Object.values(data.data);
+
+  return items;
 };
