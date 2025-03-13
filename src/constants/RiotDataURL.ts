15498d7841953fc1a6160695c44ffc454b57e652
choichangyeon <changyon99@naver.com>
2025-03-13 12:12:24 +0900

    refactor:#11 불필요한 URL 삭제

--- src/constants/RiotDataURL.ts
+++ src/constants/RiotDataURL.ts
@@ -1,14 +1,20 @@
 // 데이터 요청 URL - JSON_URL -> .json으로 요청해야 함
 
+// 버전 확인 URL
+export const LOL_REQUEST_VERSION_URL: string =
+  "https://ddragon.leagueoflegends.com/api/versions.json";
 // 로테이션 챔피언 데이터
 export const LOL_ROTATION_CHAMPION_URL: string =
   "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations";
-// 모든 챔피언 데이터
-export const LOL_CHAMPIONS_JSON_URL: string =
-  "https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion.json";
 // 특정 챔피언 데이터
 export const LOL_CHAMPION_DETAILS_JSON_URL: string =
   "https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion";
-// 모든 아이템 데이터
-export const LOL_ITEMS_JSON_URL: string =
-  "https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/item.json";
+
+// BASE_URL
+export const LOL_REQUEST_BASE_URL: string =
+  "https://ddragon.leagueoflegends.com/cdn/";
+
+export const REGION: Record<string, string> = {
+  KOR: "/ko_KR",
+  USA: "/en_US",
+};
--- src/constants/RiotImgURL.ts
+++ src/constants/RiotImgURL.ts
@@ -2,9 +2,6 @@
 // 스플래쉬 이미지 -> skins의 num으로 이미지 설정 + .jpg (ex - Aatrox_2.jpg )
 export const LOL_CHAMPION_SPLASH_IMG_URL: string =
   "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/";
-// 이미지 요청 URL
-export const LOL_IMG_URL: string =
-  "https://ddragon.leagueoflegends.com/cdn/15.5.1/img/";
 
 export const IMG_TYPE: Record<string, string> = {
   CHAMPION: "champion/",
