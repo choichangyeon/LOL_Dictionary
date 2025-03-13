18d10f52973f00ff703d7b1d5189450fc9f6b020
choichangyeon <changyon99@naver.com>
2025-03-13 11:52:09 +0900

    refactor:#11 요청 URL format

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,3 +1,30 @@
+import { LOL_REQUEST_VERSION_URL, REGION } from "@/constants/RiotDataURL";
+
 export const formatText = (originalText: string): string => {
   return originalText.replace(/<\/?[^>]+>/g, "").trim();
+};
+
+export const formatUrl = async (
+  BASE_URL: string,
+  type: string,
+  region?: keyof typeof REGION,
+): Promise<string> => {
+  const res = await fetch(LOL_REQUEST_VERSION_URL);
+  const data = await res.json();
+  const release_version = data[0];
+  switch (type) {
+    case FORMAT_TYPE.DATA:
+      return `${BASE_URL}${release_version}/data${REGION[region!]}`;
+
+    case FORMAT_TYPE.IMG:
+      return `${BASE_URL}${release_version}/img`;
+
+    default:
+      return "";
+  }
+};
+
+const FORMAT_TYPE: Record<string, string> = {
+  DATA: "data",
+  IMG: "img",
 };
