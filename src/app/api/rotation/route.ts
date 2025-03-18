ef02bf8df71e35285ed879109cdcf64002b4a637
choichangyeon <changyon99@naver.com>
2025-03-18 20:29:51 +0900

    fix:#26 변수명 수정

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export const GET = async () => {
   const res = await fetch(LOL_ROTATION_CHAMPION_URL, {
     headers: {
-      "X-Riot-Token": process.env.NEXT_PUBLIC_RIOT_API_KEY!,
+      "X-Riot-Token": process.env.RIOT_API_KEY!,
     },
   });
   const data = await res.json();
